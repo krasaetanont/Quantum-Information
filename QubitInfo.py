ade84f5dfa7b4ef904b66b3fd559daf7ae846a71
krasaetanont <krasatanontw@gmail.com>
2024-09-05 13:07:36 +0900

    Update QubitInfo.py

--- QubitInfo.py
+++ QubitInfo.py
@@ -9,6 +9,7 @@ import numpy as np
 import math as mt
 import matplotlib.pyplot as plt
 import random as rd
+import multiprocessing
 
 rho0 = np.array([0, 0])
 rhoBase = np.array([1, 0])  # Base Rho
@@ -61,31 +62,7 @@ def mutual_info(P0, P1, rho0, rho1, ang):  # Function to find mutual information
     z = np.append(z, [0])
 
     print(f"Maxima(s) of I: {maxima}, Maxima(s) of derivative of I: {derMaxima} | rho0 = {rho0}, rho1 = {rho1}")
-
-    # Rounding the variables to 3 decimal places
-    rho0 = np.array([round(rho0[0], 3), round(rho0[1], 3)])
-    rho1 = np.array([round(rho1[0], 3), round(rho1[1], 3)])
-    ang = round(ang, 3)
-    P0 = round(P0, 3)
-
-    # Plotting the graphs
-    # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-
-    # Graph 1: I and the derivative
-    # ax1.plot(x,y, label='Mutual Information I')
-    # ax1.plot(x, z, color="r", label='Derivative of I')
-    # ax1.set_title(f"P0 = {P0}, ρ0 = {rho0}, ρ1 = {rho1}")
-    # ax1.legend(loc='upper right')
-
-    # Graph 2: Sampling point of rho and initial angle
-    # circle = plt.Circle((0, 0), 1, color="r", fill=False, linestyle="--")
-    # ax2.add_patch(circle)
-    # ax2.plot(0, 0)
-    # ax2.plot([rho0[0], rho1[0]], [rho0[1], rho1[1]], color="blue")
-    # ax2.plot([0, mt.cos(a)], [0, mt.sin(a)])
-    # ax2.set_title("Sampling point of ρ and its initial measurement line")
-
-    # plt.show()
+    # plot(rho0, rho1, ang, P0, [maxima, x, y, derMaxima, z, peak])
     return [maxima, x, y, derMaxima, z, peak]
 
 
@@ -110,57 +87,64 @@ def findini(a, b):  # Function to find initial angle
 
     return best_ang
 
+# plot graph
+def plot(rho0, rho1, ang, P0, k):
+    # Rounding the variables to 3 decimal places
+    rho0 = np.array([round(rho0[0], 3), round(rho0[1], 3)])
+    rho1 = np.array([round(rho1[0], 3), round(rho1[1], 3)])
+    ang = round(ang, 3)
+    P0 = round(P0, 3)
+
+    # # Plotting the graphs
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
+    # # Graph 1: I and the derivative
+    ax1.plot(k[1], k[2], label='Mutual Information I')
+    ax1.plot(k[1], k[4], color="r", label='Derivative of I')
+    ax1.set_title(f"P0 = {round(P0, 3)}, argmax = {k[5]}")
+    # ax1.legend(loc='upper right')
+
+    # # Graph 2: Sampling point of rho and initial angle
+    circle = plt.Circle((0, 0), 1, color="r", fill=False, linestyle="--")
+    ax2.add_patch(circle)
+    ax2.plot(0, 0)
+    ax2.plot([rho0[0], rho1[0]], [rho0[1], rho1[1]], color="blue")
+    ax2.plot([0, mt.cos(ang)], [0, mt.sin(ang)])
+    ax2.set_title(f"ρ0 = {rho0}, ρ1 = {rho1}")
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+    plt.show()
+
 
 # Set a random two point
-while (True):
-    length0 = np.sqrt(np.random.uniform(0, 1))
-    angle0 = np.pi * np.random.uniform(0, 2)
-    length1 = np.sqrt(np.random.uniform(0, 1))
-    angle1 = np.pi * np.random.uniform(0, 2)
-
-    x0 = length0 * np.cos(angle0)
-    y0 = length0 * np.sin(angle0)
-    x1 = length1 * np.cos(angle1)
-    y1 = length1 * np.sin(angle1)
-    rho0 = np.array([x0, y0])
-    rho1 = np.array([x1, y1])
-    ang = findini(rho0, rho1)
-    P0 = rd.random()
-    P1 = 1 - P0
-
-    k = mutual_info(P0, P1, rho0, rho1, ang)
+def random_point():
     count = 0
-    if k[0] != 1:
-        print(k[0], "rho0 = ", rho0, "rho1 = ", rho1)
-        count += 1
-        break
-    if k[3] != 1:
-        print("Maxima(s) of I: ", k[0], "rho0 = ", rho0, "rho1 = ", rho1, "derMaxima = ", k[3], P0)
+    while (count < 100000):
+        count+= 1
 
-        # Rounding the variables to 3 decimal places
-        rho0 = np.array([round(rho0[0], 3), round(rho0[1], 3)])
-        rho1 = np.array([round(rho1[0], 3), round(rho1[1], 3)])
-        ang = round(ang, 3)
-        P0 = round(P0, 3)
+        length0 = np.sqrt(np.random.uniform(0, 1))
+        angle0 = np.pi * np.random.uniform(0, 2)
+        length1 = np.sqrt(np.random.uniform(0, 1))
+        angle1 = np.pi * np.random.uniform(0, 2)
 
-        # # Plotting the graphs
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+        x0 = length0 * np.cos(angle0)
+        y0 = length0 * np.sin(angle0)
+        x1 = length1 * np.cos(angle1)
+        y1 = length1 * np.sin(angle1)
+        rho0 = np.array([x0, y0])
+        rho1 = np.array([x1, y1])
+        ang = findini(rho0, rho1)
+        P0 = rd.random()
+        P1 = 1 - P0
 
-        # # Graph 1: I and the derivative
-        ax1.plot(k[1], k[2], label='Mutual Information I')
-        ax1.plot(k[1], k[4], color="r", label='Derivative of I')
-        ax1.set_title(f"P0 = {round(P0, 3)}, argmax = {k[5]}")
-        # ax1.legend(loc='upper right')
+        k = mutual_info(P0, P1, rho0, rho1, ang)
+        if k[0] != 1:
+            print(k[0], "rho0 = ", rho0, "rho1 = ", rho1)
+            plot(rho0, rho1, ang, P0, k)
+            break
+        # if k[3] != 1:
+        #     print("Maxima(s) of I: ", k[0], "rho0 = ", rho0, "rho1 = ", rho1, "derMaxima = ", k[3], P0)
+        #     plot(rho0, rho1, ang, P0, k)
+        #     break
 
-        # # Graph 2: Sampling point of rho and initial angle
-        circle = plt.Circle((0, 0), 1, color="r", fill=False, linestyle="--")
-        ax2.add_patch(circle)
-        ax2.plot(0, 0)
-        ax2.plot([rho0[0], rho1[0]], [rho0[1], rho1[1]], color="blue")
-        ax2.plot([0, mt.cos(ang)], [0, mt.sin(ang)])
-        ax2.set_title(f"ρ0 = {rho0}, ρ1 = {rho1}")
-        ax2.set_xticks([])
-        ax2.set_yticks([])
-        plt.show()
-        break
 
