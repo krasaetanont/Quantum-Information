4b88af9d019e297aa1e625afbb6b1bf3752d33a9
krasaetanont <krasatanontw@gmail.com>
2024-09-06 13:43:10 +0900

    Update QubitInfo.py

--- QubitInfo.py
+++ QubitInfo.py
@@ -7,7 +7,7 @@
 
 import numpy as np
 import math as mt
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import random as rd
 from multiprocessing import Process
 
@@ -15,7 +15,7 @@ rho0 = np.array([0, 0])
 rhoBase = np.array([1, 0])  # Base Rho
 
 
-def mutual_info(P0, P1, rho0, rho1, ang):  # Function to find mutual information I
+def mutual_info(P0, P1, rho0, rho1, ang, nbin):  # Function to find mutual information I
     x = np.array([])
     y = np.array([])
     z = np.array([0])
@@ -23,8 +23,8 @@ def mutual_info(P0, P1, rho0, rho1, ang):  # Function to find mutual information
     derMaxima = 0  # Maxima of derivation of I
     epsilon = 1e-10  # Small value to avoid log of zero
 
-    for i in range(101):
-        a = i * mt.pi / 100 + ang
+    for i in range(nbin + 1):
+        a = i * mt.pi / nbin + ang
 
         # Born Rule P{y|x}
         p00 = (1 + np.dot(rho0, (mt.cos(a), mt.sin(a)))) / 2
@@ -47,13 +47,13 @@ def mutual_info(P0, P1, rho0, rho1, ang):  # Function to find mutual information
             print(f"Error at angle {a}: {e}")
             continue  # Skip appending to x and y if there is an error
 
-        x = np.append(x, a / mt.pi)
+        x = np.append(x, a / mt.pi - ang / mt.pi)
         y = np.append(y, I)
 
     # Maxima detection logic with adjusted range check
     for j in range(1, len(y) - 1):
         z = np.append(z, (y[j] - y[j - 1]) / (x[j] - x[j - 1]))  # Deriving the mutual information function
-        if j > 4:
+        if j > 2:
             if z[j - 2] < z[j - 1] and z[j - 1] > z[j]:
                 derMaxima += 1
         if y[j - 1] < y[j] and y[j] > y[j + 1]:
@@ -67,60 +67,44 @@ def mutual_info(P0, P1, rho0, rho1, ang):  # Function to find mutual information
 
 
 def findini(a, b):  # Function to find initial angle
-    epsilon = 0.001
-    min_diff = float('inf')
-    best_ang = 0
-
-    # Finding the best initial alignment
-    for o in range(1001):
-        ang = o * mt.pi / 1000
-        dot_product_diff = abs(np.dot(np.array([mt.cos(ang), mt.sin(ang)]), (a - b)))
-
-        # Check if the dot product difference is closest to zero
-        if dot_product_diff < min_diff:
-            min_diff = dot_product_diff
-            best_ang = ang
-
-        # Break early if best angle is found
-        if dot_product_diff <= epsilon:
-            break
-
+    c = a-b
+    best_ang = np.arctan(-c[0]/c[1])
     return best_ang
 
 # plot graph
-def plot(rho0, rho1, ang, P0, k):
-    # Rounding the variables to 3 decimal places
-    rho0 = np.array([round(rho0[0], 3), round(rho0[1], 3)])
-    rho1 = np.array([round(rho1[0], 3), round(rho1[1], 3)])
-    ang = round(ang, 3)
-    P0 = round(P0, 3)
-
-    # # Plotting the graphs
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-
-    # # Graph 1: I and the derivative
-    ax1.plot(k[1], k[2], label='Mutual Information I')
-    ax1.plot(k[1], k[4], color="r", label='Derivative of I')
-    ax1.set_title(f"P0 = {round(P0, 3)}, argmax = {k[5]}")
-    # ax1.legend(loc='upper right')
-
-    # # Graph 2: Sampling point of rho and initial angle
-    circle = plt.Circle((0, 0), 1, color="r", fill=False, linestyle="--")
-    ax2.add_patch(circle)
-    ax2.plot(0, 0)
-    ax2.plot([rho0[0], rho1[0]], [rho0[1], rho1[1]], color="blue")
-    ax2.plot([0, mt.cos(ang)], [0, mt.sin(ang)])
-    ax2.set_title(f"ρ0 = {rho0}, ρ1 = {rho1}")
-    ax2.set_xticks([])
-    ax2.set_yticks([])
-    plt.show()
+# def plot(rho0, rho1, ang, P0, k):
+#     # Rounding the variables to 3 decimal places
+#     rho0 = np.array([round(rho0[0], 3), round(rho0[1], 3)])
+#     rho1 = np.array([round(rho1[0], 3), round(rho1[1], 3)])
+#     ang = round(ang, 3)
+#     P0 = round(P0, 3)
+#
+#     # # Plotting the graphs
+#     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+#
+#     # # Graph 1: I and the derivative
+#     ax1.plot(k[1], k[2], label='Mutual Information I')
+#     ax1.plot(k[1], k[4], color="r", label='Derivative of I')
+#     ax1.set_title(f"P0 = {round(P0, 3)}, argmax = {k[5]}")
+#     # ax1.legend(loc='upper right')
+#
+#     # # Graph 2: Sampling point of rho and initial angle
+#     circle = plt.Circle((0, 0), 1, color="r", fill=False, linestyle="--")
+#     ax2.add_patch(circle)
+#     ax2.plot(0, 0)
+#     ax2.plot([rho0[0], rho1[0]], [rho0[1], rho1[1]], color="blue")
+#     ax2.plot([0, mt.cos(ang)], [0, mt.sin(ang)])
+#     ax2.set_title(f"ρ0 = {rho0}, ρ1 = {rho1}")
+#     ax2.set_xticks([])
+#     ax2.set_yticks([])
+#     plt.show()
 
 
 # Set a random two point
-def random_point(n):
+def random_point(n, nbin):
     count = 0
     while (count < n):
-        count+= 1
+        count += 1
 
         length0 = np.sqrt(np.random.uniform(0, 1))
         angle0 = np.pi * np.random.uniform(0, 2)
@@ -137,22 +121,24 @@ def random_point(n):
         P0 = rd.random()
         P1 = 1 - P0
 
-        k = mutual_info(P0, P1, rho0, rho1, ang)
+        k = mutual_info(P0, P1, rho0, rho1, ang, nbin)
         if k[0] != 1:
-            print(k[0], "rho0 = ", rho0, "rho1 = ", rho1)
-            plot(rho0, rho1, ang, P0, k)
+            print(k[0], "rho0 = ", rho0, "rho1 = ", rho1, "P0 = ", P0)
+            # plot(rho0, rho1, ang, P0, k)
             break
-        # find maxima for the derivative of Mutual Information function
+        # for finding multiple maximum point of the derivative of I
         # if k[3] != 1:
         #     print("Maxima(s) of I: ", k[0], "rho0 = ", rho0, "rho1 = ", rho1, "derMaxima = ", k[3], P0)
         #     plot(rho0, rho1, ang, P0, k)
         #     break
 
 
-def runInParallel(nproc, num): # to run using multiple processor
+def runInParallel(nproc, num, nbin):
   proc = []
   for n in range(nproc):
-    p = Process(target=random_point, args=(num,))
+    # p = Process(target=random_point(num))
+    p = Process(target=random_point, args=(num, nbin))
+
     p.start()
     proc.append(p)
   for p in proc:
@@ -160,5 +146,5 @@ def runInParallel(nproc, num): # to run using multiple processor
 
 
 if __name__ == "__main__":
-    runInParallel(11, 100) # first argument = number of processor, second argument = number of loop
+    runInParallel(11, 1000, 1000) # first arg = number of processor, second arg = number of iteration, third arg = number of angle precision
 
